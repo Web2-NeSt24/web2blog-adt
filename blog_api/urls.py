03ac016f27c9441552f9d378b1433a0f8221e6b9
Tam-DHBW <191460049+Tam-DHBW@users.noreply.github.com>
2025-05-24 18:25:35 +0200

    Add authentication endpoints

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from blog_api import views
 
 urlpatterns = [
     path("image/<int:id>", views.ImageView.as_view()),
+    path("auth/register", views.register),
+    path("auth/login", views.login),
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,8 +1,12 @@
 from io import BytesIO
+from django.contrib import auth
 from django.http import FileResponse
+from drf_spectacular.utils import extend_schema
 from rest_framework import views
+from rest_framework.decorators import api_view
 
 from blog_api import models
+from blog_api.serializers import CredentialsSerializer
 
 class ImageView(views.APIView):
     def get(self, _request: views.Request, id: int):
@@ -12,3 +16,50 @@ class ImageView(views.APIView):
             return views.Response(status=views.status.HTTP_404_NOT_FOUND)
 
         return FileResponse(BytesIO(image.data), content_type=f"image/{image.type}")
+
+@extend_schema(request=CredentialsSerializer)
+@api_view(["POST"])
+def register(request: views.Request):
+    serializer = CredentialsSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    credentials = serializer.validated_data
+    username: str = credentials["username"].lower()
+    password: str = credentials["password"]
+
+    if not username.isalnum():
+        return views.Response({
+            "error": "Username must be alphanumeric"
+        })
+    
+    if models.User.objects.filter(username=username).exists():
+        return views.Response({
+            "error": "User already exists"
+        })
+
+    user = models.User.objects.create_user(username=username, password=password)
+
+    auth.login(request._request, user)
+
+    return views.Response(f"Created user {user.id}")
+            
+
+@extend_schema(request=CredentialsSerializer)
+@api_view(["POST"])
+def login(request: views.Request):
+    serializer = CredentialsSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    credentials = serializer.validated_data
+    username: str = credentials["username"].lower()
+    password: str = credentials["password"]
+
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        auth.login(request._request, user)
+        return views.Response()
+    else:
+        return views.Response({
+            "error": "Invalid credentials",
+        })
+
