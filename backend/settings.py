b07c64453d42a081b23c96133e9543da4b51a8b3
Tam-DHBW <191460049+Tam-DHBW@users.noreply.github.com>
2025-05-24 14:59:58 +0200

    Add image endpoint

--- backend/settings.py
+++ backend/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog_api.apps.BlogApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog_api.apps.BlogApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponse
+from io import BytesIO
+from django.http import FileResponse
+from rest_framework import views
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from blog_api import models
+
+class ImageView(views.APIView):
+    def get(self, _request: views.Request, id: int):
+        try:
+            image: models.Image = models.Image.objects.get(pk=id)
+        except models.Image.DoesNotExist:
+            return views.Response(status=views.status.HTTP_404_NOT_FOUND)
+
+        return FileResponse(BytesIO(image.data), content_type=f"image/{image.type}")
