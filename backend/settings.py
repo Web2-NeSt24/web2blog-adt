935d7a66e549eead1489487d55b91faf0c037407
Tam-DHBW <191460049+Tam-DHBW@users.noreply.github.com>
2025-05-24 13:21:56 +0200

    Port over database models

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog_api.apps.BlogApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
