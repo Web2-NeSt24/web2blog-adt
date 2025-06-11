8ed0c317953d695483cdce4cf892c09bb6fb6586
Rainer Stahl <inf24020@lehre.dhbw-stuttgart.de>
2025-06-11 12:44:41 +0200

    Merge Fix

--- blog_api/views/__init__.py
+++ blog_api/views/__init__.py
@@ -1,1 +1,13 @@
 from . import auth, bookmark, comment, draft, image, post, post_filter, profile, like
+#(fixes annoying but irrelevant error)
+__all__ = [
+    "auth",
+    "bookmark",
+    "comment",
+    "draft",
+    "image",
+    "post",
+    "post_filter",
+    "profile",
+    "like",
+]
